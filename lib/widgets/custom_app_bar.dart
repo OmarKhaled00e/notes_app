73a402d427aa969e7a7fe808d8866863db81779c
OmarKhaled00e0 <ok4966541@gmail.com>
2025-04-11 19:23:53 +0200

    customApp and custom Icon

---
+++ lib/widgets/custom_app_bar.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text('Notes', style: TextStyle(fontSize: 28),
+        ),
+        Spacer(),
+        CustomAppBar(),
+      ],
+    );
+  }
+}
