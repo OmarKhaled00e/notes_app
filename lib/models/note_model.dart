05a595c76e1d195c237723c6c2f5508c92cc4e0a
OmarKhaled00e0 <ok4966541@gmail.com>
2025-04-12 22:43:10 +0200

    add file build_runner .g

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,8 +1,23 @@
-class NoteModel {
+import 'package:hive/hive.dart';
+
+
+part 'note_model.g.dart';
+
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String subTitle;
+  @HiveField(2)
   final int color;
+  @HiveField(3)
   final String date;
 
-  NoteModel({required this.date, required this.title, required this.subTitle, required this.color});
+  NoteModel({
+    required this.date,
+    required this.title,
+    required this.subTitle,
+    required this.color,
+  });
 }
