0b8271c09d5df4c2ff2cf8903ec578aee1c8f79c
OmarKhaled00e0 <ok4966541@gmail.com>
2025-04-14 03:22:19 +0200

    edit note done

--- lib/widgets/custom_text_filed.dart
+++ lib/widgets/custom_text_filed.dart
@@ -6,14 +6,16 @@ class CustomTextFiled extends StatelessWidget {
     super.key,
     required this.hint,
     this.maxLines = 1,
-    this.onSaved,
+    this.onSaved, this.onChanged,
   });
   final String hint;
   final int maxLines;
   final void Function(String?)? onSaved;
+  final void Function(String)? onChanged;
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      onChanged: onChanged,
       onSaved: onSaved,
       validator: (value) {
         if (value?.isEmpty ?? true) {
--- lib/widgets/edit_note_view_body.dart
+++ lib/widgets/edit_note_view_body.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
+import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/widgets/custom_app_bar.dart';
 import 'package:notes_app/widgets/custom_text_filed.dart';
 
-class EditNoteViewBody extends StatelessWidget {
-  const EditNoteViewBody({super.key});
+class EditNoteViewBody extends StatefulWidget {
+  const EditNoteViewBody({super.key, required this.note});
+  final NoteModel note;
 
+  @override
+  State<EditNoteViewBody> createState() => _EditNoteViewBodyState();
+}
+
+class _EditNoteViewBodyState extends State<EditNoteViewBody> {
+  String? title, content;
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -12,12 +22,35 @@ class EditNoteViewBody extends StatelessWidget {
       child: Column(
         children: [
           SizedBox(height: 50),
-          CustomAppBar(title: ' Edit Note', icon: Icons.check),
-          SizedBox(height: 50,),
-          CustomTextFiled(hint: 'Title'),
-          SizedBox
-          (height: 16,),
-          CustomTextFiled(hint: 'Content', maxLines: 5,)
+          CustomAppBar(
+            onPressed: () {
+              widget.note.title = title ?? widget.note.title;
+              widget.note.subTitle = content ?? widget.note.subTitle;
+              widget.note.save();
+              BlocProvider.of<NotesCubit>(context).fatchAllNotes();
+              Navigator.pop(context);
+              ScaffoldMessenger.of(
+                context,
+              ).showSnackBar(SnackBar(content: Text('Edit note Success')));
+            },
+            title: ' Edit Note',
+            icon: Icons.check,
+          ),
+          SizedBox(height: 50),
+          CustomTextFiled(
+            onChanged: (value) {
+              title = value;
+            },
+            hint: widget.note.title,
+          ),
+          SizedBox(height: 16),
+          CustomTextFiled(
+            onChanged: (value) {
+              content = value;
+            },
+            hint: widget.note.subTitle,
+            maxLines: 5,
+          ),
         ],
       ),
     );
